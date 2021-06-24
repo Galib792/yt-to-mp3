d46f84edfa2c2c779c6db2a2bf6cdca7e8e3f3ea
eo792 <85933934+eo792@users.noreply.github.com>
2021-06-24 17:27:18 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -66,7 +66,7 @@ class Chat:
     def __init__(self, msg):
         self.chat_id = msg['chat']['id']
         self.user_input = msg['text']
-        self.user_input = self.user_input.replace('@TLMusicDownloader_bot', '')
+        self.user_input = self.user_input.replace('@MusicDownloader_TT_Bot', '')
         self.user_name = msg['from']['first_name']
         self.message_id = msg['message_id']
 
