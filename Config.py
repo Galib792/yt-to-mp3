526ee573c7b5876ae350e487d607c1cb810ad052
Galib792 <85933934+Galib792@users.noreply.github.com>
2021-07-08 07:08:30 +0530

    Create Config.py

---
+++ Config.py
@@ -0,0 +1,7 @@
+import os
+
+class Config():
+  #Get it from @botfather
+  BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+  # Your bot updates channel username without @ or leave empty
+  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
